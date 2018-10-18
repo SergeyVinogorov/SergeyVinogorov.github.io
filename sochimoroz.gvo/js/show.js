d3bff1f6116681f8fbaba587b6764db3b7f44f82
Sergey Vinogorov <Vinogorich@gmail.com>
2018-10-18 16:32:31 +0300

    for portfolio

---
+++ sochimoroz.gvo/js/show.js
@@ -0,0 +1,584 @@
+$('#open-popup').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Barman/Barman1-min.jpg',
+        title: 'Шоу барменов'
+      },
+      {
+        src: 'img/show/Barman/Barman2-min.jpg',
+        title: 'Шоу барменов'
+      },
+      {
+        src: 'img/show/Barman/Barman3-min.jpg',
+        title: 'Шоу барменов'
+      },
+      {
+        src: 'img/show/Barman/Barman4-min.jpg',
+        type: 'iframe' // this overrides default type
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});//Barman
+
+$('#open-popup1').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Brazil/Brazil1-min.jpg',
+      },
+      {
+        src: 'img/show/Brazil/Brazil2-min.jpg',
+      },
+      {
+        src: 'img/show/Brazil/Brazil4-min.jpg',
+      },
+      {
+        src: 'img/show/Brazil/Brazil5-min.jpg',
+      },
+      {
+        src: 'img/show/Brazil/Brazil6-min.jpg',
+      },
+      {
+        src: 'img/show/Brazil/Brazil7-min.jpg',
+      },
+      {
+        src: 'img/show/Brazil/Brazil9-min.jpg',
+      },
+      {
+        src: 'img/show/Brazil/Brazil10-min.jpg',
+        type: 'iframe'
+      },
+    ],//
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Brazil
+
+$('#open-popup2').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Diana/Diana2-min.jpg',
+      },
+      {
+        src: 'img/show/Diana/Diana3-min.jpg',
+      },
+      {
+        src: 'img/show/Diana/Diana5-min.jpg',
+      },
+      {
+        src: 'img/show/Diana/Diana7-min.jpg',
+      },
+      {
+        src: 'img/show/Diana/Diana8-min.jpg',
+      },
+      {
+        src: 'img/show/Diana/Diana9-min.jpg',
+        type: 'iframe'
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Diana
+
+$('#open-popup3').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Drammer/Drammer1-min.jpg',
+      },
+      {
+        src: 'img/show/Drammer/Drammer2-min.jpg',
+      },
+      {
+        src: 'img/show/Drammer/Drammer3-min.jpg',
+      },
+      {
+        src: 'img/show/Drammer/Drammer4-min.jpg',
+      },
+      {
+        src: 'img/show/Drammer/Drammer5-min.jpg',
+        type: 'iframe' // this overrides default type
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});//Drammer
+
+//line
+
+$('#open-popup4').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Fakhir/Fakhir1-min.jpg',
+      },
+      {
+        src: 'img/show/Fakhir/Fakhir2-min.jpg',
+      },
+      {
+        src: 'img/show/Fakhir/Fakhir3-min.jpg',
+      },
+      {
+        src: 'img/show/Fakhir/Fakhir4-min.jpg',
+      },
+      {
+        src: 'img/show/Fakhir/Fakhir5-min.jpg',
+      },
+      {
+        src: 'img/show/Fakhir/Fakhir6-min.jpg',
+      },
+      {
+        src: 'img/show/Fakhir/Fakhir7-min.jpg',
+        type: 'iframe'
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Fakhir
+$('#open-popup5').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Flamingo/Flamingo1-min.jpg',
+      },
+      {
+        src: 'img/show/Flamingo/Flamingo2-min.jpg',
+      },
+      {
+        src: 'img/show/Flamingo/Flamingo3-min.jpg',
+      },
+      {
+        src: 'img/show/Flamingo/Flamingo4-min.jpg',
+        type: 'iframe'
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Flamingo
+$('#open-popup6').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Illusion/Illusion1-min.jpg',
+      },
+      {
+        src: 'img/show/Illusion/Illusion2-min.jpg',
+      },
+      {
+        src: 'img/show/Illusion/Illusion3-min.jpg',
+      },
+      {
+        src: 'img/show/Illusion/Illusion4-min.jpg',
+      },
+      {
+        src: 'img/show/Illusion/Illusion5-min.jpg',
+      },
+      {
+        src: 'img/show/Illusion/Illusion6-min.jpg',
+      },
+      {
+        src: 'img/show/Illusion/Illusion7-min.jpg',
+        type: 'iframe'
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Illusion
+$('#open-popup7').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Night/Night1-min.jpg',
+      },
+      {
+        src: 'img/show/Night/Night2-min.jpg',
+      },
+      {
+        src: 'img/show/Night/Night3-min.jpg',
+      },
+      {
+        src: 'img/show/Night/Night4-min.jpg',
+      },
+      {
+        src: 'img/show/Night/Night5-min.jpg',
+      },
+      {
+        src: 'img/show/Night/Night0-min.jpg',
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Night
+
+//Line
+
+$('#open-popup8').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Nika/Nika1-min.jpg',
+      },
+      {
+        src: 'img/show/Nika/Nika2-min.jpg',
+      },
+      {
+        src: 'img/show/Nika/Nika3-min.jpg',
+      },
+      {
+        src: 'img/show/Nika/Nika4-min.jpg',
+      },
+      {
+        src: 'img/show/Nika/Nika5-min.jpg',
+      },
+      {
+        src: 'img/show/Nika/Nika6-min.jpg',
+      },
+      {
+        src: 'img/show/Nika/Nika7-min.jpg',
+      },
+      {
+        src: 'img/show/Nika/Nika8-min.jpg',
+      },
+      {
+        src: 'img/show/Nika/Nika9-min.jpg',
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Nika
+$('#open-popup9').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Picture/Picture1-min.jpg',
+      },
+      {
+        src: 'img/show/Picture/Picture2-min.jpg',
+      },
+      {
+        src: 'img/show/Picture/Picture3-min.jpg',
+      },
+      {
+        src: 'img/show/Picture/Picture4-min.jpg',
+      },
+      {
+        src: 'img/show/Picture/Picture5-min.jpg',
+      },
+      {
+        src: 'img/show/Picture/Picture6-min.jpg',
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Picture
+$('#open-popup10').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Prody/Parody1-min.jpg',
+      },
+      {
+        src: 'img/show/Prody/Parody2-min.jpg',
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Prody
+$('#open-popup11').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Sand/Sand1-min.jpg',
+      },
+      {
+        src: 'img/show/Sand/Sand2-min.jpg',
+      },
+      {
+        src: 'img/show/Sand/Sand3-min.jpg',
+      },
+      {
+        src: 'img/show/Sand/Sand4-min.jpg',
+      },
+      {
+        src: 'img/show/Sand/Sand5-min.jpg',
+      },
+      {
+        src: 'img/show/Sand/Sand6-min.jpg',
+      },
+      {
+        src: 'img/show/Sand/Sand7-min.jpg',
+      },
+      {
+        src: 'img/show/Sand/Sand8-min.jpg',
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Sand
+
+//Line
+
+$('#open-popup12').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Serduchka/Serduchka1-min.jpg',
+      },
+      {
+        src: 'img/show/Serduchka/Serduchka2-min.jpg',
+      },
+      {
+        src: 'img/show/Serduchka/Serduchka3-min.jpg',
+      },
+      {
+        src: 'img/show/Serduchka/Serduchka4-min.jpg',
+      },
+      {
+        src: 'img/show/Serduchka/Serduchka5-min.jpg',
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Serduchka
+$('#open-popup13').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Spring/Spring1-min.jpg',
+      },
+      {
+        src: 'img/show/Spring/Spring2-min.jpg',
+      },
+      {
+        src: 'img/show/Spring/Spring3-min.jpg',
+      },
+      {
+        src: 'img/show/Spring/Spring4-min.jpg',
+      },
+      {
+        src: 'img/show/Spring/Spring5-min.jpg',
+      },
+      {
+        src: 'img/show/Spring/Spring6-min.jpg',
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Spring
+$('#open-popup14').magnificPopup({
+    items: [
+      {
+        src: 'img/show/TAJ/TAJ1-min.jpg',
+      },
+      {
+        src: 'img/show/TAJ/TAJ2-min.jpg',
+      },
+      {
+        src: 'img/show/TAJ/TAJ3-min.jpg',
+      },
+      {
+        src: 'img/show/TAJ/TAJ4-min.jpg',
+      },
+      {
+        src: 'img/show/TAJ/TAJ5-min.jpg',
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//TAJ
+$('#open-popup15').magnificPopup({
+    items: [
+      {
+        src: 'img/PaperShow/PaperShow_1.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_2.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_3.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_4.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_5.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_6.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_7.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_8.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_9.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_10.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_11.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_12.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_13.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_14.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_15.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_16.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_17.jpg',
+      },
+      {
+        src: 'img/PaperShow/PaperShow_18.jpg',
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Paper
+
+//Line
+
+$('#open-popup16').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Сartoonist/Сartoonist1-min.jpg',
+      },
+      {
+        src: 'img/show/Сartoonist/Сartoonist2-min.jpg',
+      },
+      {
+        src: 'img/show/Сartoonist/Сartoonist3-min.jpg',
+      },
+      {
+        src: 'img/show/Сartoonist/Сartoonist4-min.jpg',
+      },
+      {
+        src: 'img/show/Сartoonist/Сartoonist5-min.jpg',
+      },
+      {
+        src: 'img/show/Сartoonist/Сartoonist6-min.jpg',
+      },
+      {
+        src: 'img/show/Сartoonist/Сartoonist7-min.jpg',
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Cartonist
+$('#open-popup17').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Vasili/Vasili1-min.jpg',
+      },
+      {
+        src: 'img/show/Vasili/Vasili2-min.jpg',
+      },
+      {
+        src: 'img/show/Vasili/Vasili3-min.jpg',
+      },
+      {
+        src: 'img/show/Vasili/Vasili4-min.jpg',
+      },
+      {
+        src: 'img/show/Vasili/Vasili5-min.jpg',
+      },
+      {
+        src: 'img/show/Vasili/Vasili6-min.jpg',
+      },
+      {
+        src: 'img/show/Vasili/Vasili7-min.jpg',
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Vasili
+$('#open-popup18').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Shantal/Shantal1-min.jpg',
+      },
+      {
+        src: 'img/show/Shantal/Shantal2-min.jpg',
+      },
+      {
+        src: 'img/show/Shantal/Shantal3-min.jpg',
+      },
+      {
+        src: 'img/show/Shantal/Shantal4-min.jpg',
+      },
+      {
+        src: 'img/show/Shantal/Shantal5-min.jpg',
+      },
+      {
+        src: 'img/show/Shantal/Shantal6-min.jpg',
+      },
+      {
+        src: 'img/show/Shantal/Shantal7-min.jpg',
+      },
+      {
+        src: 'img/show/Shantal/Shantal8-min.jpg',
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Shantal
+$('#open-popup19').magnificPopup({
+    items: [
+      {
+        src: 'img/show/Saxophonist/Saxophonist1-min.jpg',
+      },
+      {
+        src: 'img/show/Saxophonist/Saxophonist3-min.jpg',
+      },
+      {
+        src: 'img/show/Saxophonist/Saxophonist4-min.jpg',
+      },
+      {
+        src: 'img/show/Saxophonist/Saxophonist5-min.jpg',
+      },
+      {
+        src: 'img/show/Saxophonist/Saxophonist6-min.jpg',
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image'
+});//Saxophonist
