4d42aacb20733a44ce9b0b0158fe33aa6ff71a54
Sergey Vinogorov <vinogorich@gmail.comgit config --global user.email vinogorich@gmail.comgit config --global user.email vinogorich@gmail.com>
2020-03-14 22:10:36 +0300

    add Launch component

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Launches from "./component/Launches";
+import Launch from "./component/Launch";
+
+import "./App.css";
+import logo from "./logo.png";
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="container">
+          <img
+            src={logo}
+            alt="SpaceX"
+            style={{ width: 300, display: "block", margin: "auto" }}
+          />
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
