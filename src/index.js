75edf9e2056117311c9d16655dfdcb5c76638634
Sergey Vinogorov <vinogorich@gmail.comgit config --global user.email vinogorich@gmail.comgit config --global user.email vinogorich@gmail.com>
2020-04-04 16:15:36 +0300

    add UserPopVue

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import UserPop from "./UserPop.vue";
+import UserPopImage from "./UserPopImage.vue";
 
-export default {
-  install(Vue, options) {
+const previewComponent = {
+  install: function (Vue, options) {
     Vue.component("user-pop", UserPop);
+    Vue.component("user-pop-image", UserPopImage);
   },
 };
+
+export default previewComponent;
